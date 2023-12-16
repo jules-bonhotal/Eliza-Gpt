5e0bca2daac0292970c75dfaefdda97ebc8d6f60
Oussama Benaziz <oussama.benaziz@etu.univ-lyon1.fr>
2023-12-16 19:48:02 +0100

    amelioration de l'interface et rajout theme sombre

--- eliza-gpt/src/main/java/fr/univ_lyon1/info/m1/elizagpt/view/JfxView.java
+++ eliza-gpt/src/main/java/fr/univ_lyon1/info/m1/elizagpt/view/JfxView.java
@@ -1,18 +1,9 @@
 package fr.univ_lyon1.info.m1.elizagpt.view;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
-
-import fr.univ_lyon1.info.m1.elizagpt.model.MessageObserver;
-import fr.univ_lyon1.info.m1.elizagpt.model.MessageStorage;
-import fr.univ_lyon1.info.m1.elizagpt.model.Message;
-import fr.univ_lyon1.info.m1.elizagpt.model.SearchStrategyList;
-import fr.univ_lyon1.info.m1.elizagpt.model.SearchStrategy;
-
-import fr.univ_lyon1.info.m1.elizagpt.controller.Controller;
-
+import javafx.geometry.Insets; // Ajout de cette importation
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -21,9 +12,19 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.util.List;
 import java.util.Random;
+
+import fr.univ_lyon1.info.m1.elizagpt.controller.Controller;
+import fr.univ_lyon1.info.m1.elizagpt.model.Message;
+import fr.univ_lyon1.info.m1.elizagpt.model.MessageObserver;
+import fr.univ_lyon1.info.m1.elizagpt.model.MessageStorage;
+import fr.univ_lyon1.info.m1.elizagpt.model.SearchStrategy;
+import fr.univ_lyon1.info.m1.elizagpt.model.SearchStrategyList;
 import javafx.collections.FXCollections;
 import javafx.scene.control.ComboBox;
 
@@ -35,29 +36,31 @@ public class JfxView implements MessageObserver {
     private TextField text = null;
     private TextField searchText = null;
     private Label searchTextLabel = null;
-    // private MessageProcessor processor;
-    // private MessageStorage messageStorage;
     private Controller controller;
     private final Random random = new Random();
+    private boolean isDarkMode = false;
+    private VBox root;
+
     /**
-     * Create the main view of the application.
+     * Constructs a new instance of {@code JfxView}.
+     *
+     * @param stage          The primary stage for the JavaFX application.
+     * @param width          The width of the scene.
+     * @param height         The height of the scene.
+     * @param messageStorage The message storage to be observed.
+     * @param controller     The controller for handling user interactions.
      */
     public JfxView(final Stage stage,
-            final int width,
-            final int height,
-            final MessageStorage messageStorage,
-            // final MessageProcessor messageProcessor,
-            final Controller controller) {
-        //s'ajoute en temps qu'observer du stockage des message pour etre notifier des changements
-        // this.messageStorage = messageStorage;
+                   final int width,
+                   final int height,
+                   final MessageStorage messageStorage,
+                   final Controller controller) {
         messageStorage.registerObserver(this);
-
-        // processor = messageProcessor;
         this.controller = controller;
 
-        stage.setTitle("Eliza GPT");
+        root = new VBox(10);
 
-        final VBox root = new VBox(10);
+        stage.setTitle("Eliza GPT");
 
         final Pane search = createSearchWidget();
         root.getChildren().add(search);
@@ -65,7 +68,6 @@ public class JfxView implements MessageObserver {
         ScrollPane dialogScroll = new ScrollPane();
         dialog = new VBox(10);
         dialogScroll.setContent(dialog);
-        // scroll to bottom by default:
         dialogScroll.vvalueProperty().bind(dialog.heightProperty());
         root.getChildren().add(dialogScroll);
         dialogScroll.setFitToWidth(true);
@@ -73,7 +75,16 @@ public class JfxView implements MessageObserver {
         final Pane input = createInputWidget();
         root.getChildren().add(input);
 
-        // Everything's ready: add it to the scene and display it
+        updateTheme();
+
+        Button themeToggleButton = new Button("Toggle Theme");
+        themeToggleButton.setOnAction(e -> toggleTheme());
+        HBox.setHgrow(themeToggleButton, Priority.ALWAYS);
+        HBox.setMargin(themeToggleButton, new Insets(10, 10, 10, 10));
+        HBox bottomRight = new HBox(themeToggleButton);
+        bottomRight.setAlignment(Pos.BOTTOM_RIGHT);
+        root.getChildren().add(bottomRight);
+
         final Scene scene = new Scene(root, width, height);
         stage.setScene(scene);
         text.requestFocus();
@@ -81,65 +92,94 @@ public class JfxView implements MessageObserver {
         update("start");
     }
 
+    private void updateTheme() {
+        String backgroundColor = isDarkMode ? "#333333" : "#FFFFFF";
+        String otherColor = isDarkMode ? "#666666" : "#DDDDDD";
+        String textColor = isDarkMode ? "#FFFFFF" : "#000000"; // Couleur du texte
+
+        // Mise à jour du style du root
+        root.setStyle("-fx-background-color: " + backgroundColor + "; -fx-text-fill: " + textColor);
+
+        // Mise à jour du style des champs de texte
+        if (text != null) {
+            text.setStyle("-fx-background-color: " + otherColor + "; -fx-text-fill: " + textColor);
+        }
+
+        if (searchText != null) {
+            searchText.setStyle("-fx-background-color: " 
+                + otherColor + "; -fx-text-fill: " + textColor);
+        }
+
+        // Mise à jour du style de la boîte de dialogue
+        dialog.setStyle("-fx-background-color: " + otherColor); 
+    }
+
+    private void toggleTheme() {
+        isDarkMode = !isDarkMode;
+        updateTheme();
+    }
 
     /**
-     * Updates the graphical interface based on the notification received.
+     * Updates the view based on the provided notification.
+     * Clears the existing dialog content and populates it with messages from the controller.
      *
-     * @param notification The notification string indicating the type of update.
+     * @param notification A string indicating the type of notification.
      */
     public void update(final String notification) {
-        // Clear existing HBox elements
         dialog.getChildren().clear();
-
-        // Get the messages from the MessageStorage
         List<Message> messages = controller.getMessages();
 
-        // Create HBox elements based on the messages
         for (Message message : messages) {
             HBox hBox;
             if (message.isUserMessage()) {
                 hBox = createHBoxWithLabel(message.getMessageText(),
-                            USER_STYLE,
-                            message.getMessageId());
+                        USER_STYLE,
+                        message.getMessageId());
             } else {
                 hBox = createHBoxWithLabel(message.getMessageText(),
-                            ELIZA_STYLE,
-                            message.getMessageId());
+                        ELIZA_STYLE,
+                        message.getMessageId());
             }
             dialog.getChildren().add(hBox);
         }
     }
 
-
-
-
     static final String BASE_STYLE = "-fx-padding: 8px; "
             + "-fx-margin: 5px; "
             + "-fx-background-radius: 5px;";
-    static final String USER_STYLE = "-fx-background-color: #A0E0A0; " + BASE_STYLE;
-    static final String ELIZA_STYLE = "-fx-background-color: #A0A0E0; " + BASE_STYLE;
+    static final String USER_STYLE = "-fx-background-color:  #94DB96; " + BASE_STYLE;
+    static final String ELIZA_STYLE = "-fx-background-color: #73C2FB; " + BASE_STYLE;
+    static final String BUTTON_STYLE = "-fx-background-color: #4CAF50; -fx-text-fill: white;";
+    static final String DELETE_BUTTON_STYLE = 
+                                        "-fx-background-color: #FF6666; -fx-text-fill: white;";
+    static final String UNDO_BUTTON_STYLE = "-fx-background-color: #FF6666; -fx-text-fill: white;";
 
-    // Factorize the creation of HBox with label and button
     private HBox createHBoxWithLabel(final String text,
-            final String style,
-            final String messageId) {
+                                  final String style,
+                                  final String messageId) {
         HBox outerHBox = new HBox();
         HBox innerHBox = new HBox();
-        final Label label = new Label(text);
+        final Label label = new Label(" " + text + " ");
         final Button deleteButton = new Button("x");
+        setButtonStyle(deleteButton, DELETE_BUTTON_STYLE);
 
         if (style.equals(USER_STYLE)) {
-            innerHBox.getChildren().addAll(deleteButton, label);
+            innerHBox.getChildren().addAll(label, deleteButton);
             outerHBox.setAlignment(Pos.BASELINE_RIGHT);
-            // deleteButton.setAlignment(Pos.BASELINE_LEFT);
         } else {
             innerHBox.getChildren().addAll(label, deleteButton);
             outerHBox.setAlignment(Pos.BASELINE_LEFT);
-            // deleteButton.setAlignment(Pos.BASELINE_RIGHT);
         }
 
         innerHBox.setStyle(style);
-        outerHBox.getChildren().add(innerHBox);
+
+        // Centrer le texte dans innerHBox
+        innerHBox.setAlignment(Pos.CENTER);
+
+        outerHBox.getChildren().addAll(innerHBox, new Pane());
+        outerHBox.setSpacing(10);
+
+        outerHBox.setStyle("-fx-padding: 0 10 0 10;");
 
         outerHBox.setId(messageId);
 
@@ -151,29 +191,26 @@ public class JfxView implements MessageObserver {
         return outerHBox;
     }
 
-
     private HBox createHBoxWithLabel(final String text, final String style) {
         String uniqueId = UUID.randomUUID().toString();
         return createHBoxWithLabel(text, style, uniqueId);
     }
 
-
-
-
-
-
+    /**
+     * Creates the search widget.
+     *
+     * @return the created search widget
+     */
     private Pane createSearchWidget() {
         final HBox firstLine = new HBox();
         final HBox secondLine = new HBox();
         firstLine.setAlignment(Pos.BASELINE_LEFT);
         secondLine.setAlignment(Pos.BASELINE_LEFT);
 
-
-        // Create a ComboBox for search options
         SearchStrategyList searchStrategyList = new SearchStrategyList();
         ArrayList<SearchStrategy> searchOptions = searchStrategyList.getList();
 
-        ComboBox<SearchStrategy> searchOptionsComboBox = 
+        ComboBox<SearchStrategy> searchOptionsComboBox =
                 new ComboBox<>(FXCollections.observableArrayList(searchOptions));
         searchOptionsComboBox.setPromptText("Select Search Option");
 
@@ -182,46 +219,42 @@ public class JfxView implements MessageObserver {
             SearchStrategy selectedOption = searchOptionsComboBox.getValue();
             String regexPattern = searchText.getText();
 
-            // Utilisation de la stratégie sélectionnée pour la recherche
             if (selectedOption != null) {
                 List<Message> matchingMessages = controller.executeSearchController(
-                    selectedOption,
-                    regexPattern);
+                        selectedOption,
+                        regexPattern);
                 updateSearchResults(matchingMessages);
             }
         });
 
         firstLine.getChildren().addAll(searchText, searchOptionsComboBox);
         final Button send = new Button("Search");
+        setButtonStyle(send, BUTTON_STYLE);
         send.setOnAction(e -> {
-            SearchStrategy selectedOption = searchOptionsComboBox.getValue();
-            String regexPattern = searchText.getText();
-
-            // Utilisation de la stratégie sélectionnée pour la recherche
-            if (selectedOption != null) {
-                List<Message> matchingMessages = controller.executeSearchController(
-                                    selectedOption,
-                                    regexPattern);
-                updateSearchResults(matchingMessages);
-            }
+            controller.sendMessage(text.getText());
+            text.setText("");
         });
 
         searchTextLabel = new Label();
         final Button undo = new Button("Undo search");
+        setButtonStyle(undo, UNDO_BUTTON_STYLE);
         undo.setOnAction(e -> {
-            // Clear the search text and update the view
             searchText.clear();
             update("start");
         });
         secondLine.getChildren().addAll(send, searchTextLabel, undo);
+        secondLine.setSpacing(10);
+
         final VBox input = new VBox();
         input.getChildren().addAll(firstLine, secondLine);
         return input;
     }
 
-
-
-    // update the search results in the View
+    /**
+     * Updates the search results.
+     *
+     * @param matchingMessages the matching messages
+     */
     private void updateSearchResults(final List<Message> matchingMessages) {
         dialog.getChildren().clear();
 
@@ -239,7 +272,6 @@ public class JfxView implements MessageObserver {
             dialog.getChildren().add(hBox);
         }
 
-        // Update the search label
         String currentSearchText = searchText.getText();
         if (currentSearchText == null || currentSearchText.isEmpty()) {
             searchTextLabel.setText("No active search");
@@ -248,52 +280,25 @@ public class JfxView implements MessageObserver {
         }
     }
 
-
-
-
-
-
-/**
- * Crée un widget d'entrée pour l'utilisateur.
- *
- * Ce widget permet à l'utilisateur de saisir du texte dans un champ de texte
- * et d'envoyer ce texte en appuyant sur la touche "Enter" ou en cliquant sur
- * le bouton "Send". Lorsque l'utilisateur envoie un message, le texte saisi est
- * transmis à la fonction sendMessage, et le champ de texte est effacé pour
- * permettre la saisie d'un nouveau message.
- *
- * @return Un conteneur d'interface utilisateur (HBox) comprenant un champ de texte
- *         et un bouton "Send" pour l'envoi de messages.
- */
-
     private Pane createInputWidget() {
         final Pane input = new HBox();
         text = new TextField();
         text.setOnAction(e -> {
-            // TODO : voire si il faut pas que ca passe par le contoller 
-            // processor.sendMessage(text.getText());
             controller.sendMessage(text.getText());
             text.setText("");
         });
         final Button send = new Button("Send");
+        setButtonStyle(send, BUTTON_STYLE);
         send.setOnAction(e -> {
-            // TODO : voire si il faut pas que ca passe par le contoller 
-            // processor.sendMessage(text.getText());
             controller.sendMessage(text.getText());
             text.setText("");
         });
         input.getChildren().addAll(text, send);
+        ((HBox) input).setSpacing(10); 
         return input;
     }
 
-    // //  utility class for managing strategies 
-    // // TODO temporary solution, there is most likly a better solution
-    // public class SearchStrategyUtil {
-    //     public static List<SearchStrategy> getAllStrategiesList() {
-    //         List<SearchStrategy> strategies = new ArrayList<>();
-    //         strategies.add(new RegexSearch());
-    //         strategies.add(new Option2Search());
-    //         return strategies;
-    //     }
-    // }
+    private void setButtonStyle(final Button button, final String style) {
+        button.setStyle(style);
+    }
 }
